7f83af5a7f05b2e25dde631cffcb33632d97ac8b
Robert Olofsson <robo@khelekore.org>
2013-02-09 17:22:07 +0100

    Made it possible to configure tcp_no_delay on or off.

--- src/rabbit/proxy/ProxyConnectionAcceptor.java
+++ src/rabbit/proxy/ProxyConnectionAcceptor.java
@@ -6,16 +6,18 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.khelekore.rnio.impl.AcceptorListener;
+import rabbit.util.Config;
 
 /** An acceptor handler that creates proxy client connection
  *
  * @author <a href="mailto:robo@khelekore.org">Robert Olofsson</a>
  */
 public class ProxyConnectionAcceptor implements AcceptorListener {
-    private final int id;
     private final HttpProxy proxy;
     private final Logger logger = Logger.getLogger (getClass ().getName ());
     private final AtomicLong counter = new AtomicLong ();
+    private final int id;
+    private final boolean setTcpNoDelay;
 
     /** Create a new ProxyConnectionAcceptor.
      * @param id the connection group id
@@ -25,6 +27,10 @@ public class ProxyConnectionAcceptor implements AcceptorListener {
 	logger.fine ("ProxyConnectionAcceptor created: " + id);
 	this.id = id;
 	this.proxy = proxy;
+	Config c = proxy.getConfig ();
+	String tcpNoDelay = c.getProperty (HttpProxy.class.getName (),
+					   "use_tcp_no_delay", "false");
+	setTcpNoDelay = "true".equalsIgnoreCase (tcpNoDelay);
     }
 
     public void connectionAccepted (SocketChannel sc) 
@@ -38,6 +44,8 @@ public class ProxyConnectionAcceptor implements AcceptorListener {
 	    proxy.getCounter ().inc ("Rejected IP:s");
 	    sc.close ();
 	} else {
+	    if (setTcpNoDelay)
+		sc.socket ().setTcpNoDelay (true);
 	    Connection c = new Connection (getId (), sc, proxy);
 	    c.readRequest ();
 	}
