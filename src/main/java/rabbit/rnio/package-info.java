9d8f1b110c5e518588293bdd9467acf37f6e4764
Nathan Toone <nathan@toonetown.com>
2014-02-27 15:26:11 -0700

    Remove javadoc stuff (I don't like these)

--- src/main/java/rabbit/rnio/package-info.java
+++
@@ -1,7 +0,0 @@
-/** The basic interfaces for rnio.
- *
- *  <p>The main class here is the {@link rabbit.rnio.NioHandler}.
- *
- * @since 1.0
- */
-package rabbit.rnio;
--- src/main/java/rabbit/rnio/statistics/package-info.java
+++
@@ -1,5 +0,0 @@
-/** Statistics information about rnio.
- *
- * @since 1.0
- */
-package rabbit.rnio.statistics;
